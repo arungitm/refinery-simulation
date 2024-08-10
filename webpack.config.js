aa52dea7f1e3089df56b8e63d8a5e9ca27425ee3
arungitm <arunmozhi.manickam@gmail.com>
2024-08-10 08:48:29 +0530

    Add files via upload
    new

--- webpack.config.js
+++ webpack.config.js
@@ -1,35 +1,3 @@
-<<<<<<< HEAD
-const path = require('path');
-
-module.exports = {
-  entry: './refinery-simulation.js',
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-    publicPath: '/refinery-simulation/dist/'
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env'],
-          },
-        },
-      },
-    ],
-  },
-  resolve: {
-    extensions: ['.js'],
-    alias: {
-      'three': path.resolve(__dirname, 'node_modules/three')
-    }
-  },
-};
-=======
 const path = require('path');
 
 module.exports = {
@@ -60,4 +28,3 @@ module.exports = {
     }
   },
 };
->>>>>>> origin/main
